7bac784e2f8980fd896d514adbde0a69c7cd5f1a
ProjektKris <rbxkrisxyz@gmail.com>
2021-05-22 14:39:32 +0700

    added tester and test1

--- src/server/init.server.lua
+++ src/server/init.server.lua
@@ -1,1 +1,32 @@
-print("Hello world, from server!")
+--!nonstrict
+--[[
+    Author: @ProjektKris
+    serverside tester
+]]
+
+type Array<T> = {[number]: T}
+type Result = {
+    t0: number,
+    t1: number,
+    module: ModuleScript
+}
+
+local results: Array<Result> = {}
+for _, module: Instance in pairs(script:GetChildren()) do
+    if module:IsA("ModuleScript") then
+        local newResult: Result
+        newResult.t0 = os.clock()
+
+        require(module)()
+
+        newResult.t1 = os.clock()
+        newResult.module = module
+
+        results[#results+1] = newResult
+    end
+end
+
+-- summarize
+for _, res: Result in pairs(results) do
+    print(string.format("%s finished in %f", res.module.name, res.t1-res.t0))
+end
---
+++ src/server/test1.lua
@@ -0,0 +1,22 @@
+return function ()
+    local pointer = require(game.ReplicatedStorage.Common.pointer)
+
+    local newPointerVar: pointer.Pointer, err: string = pointer.New(69)
+    if err ~= nil then
+        warn(err)
+    end
+
+    local point: pointer.Pointer, err2: string = pointer.GetPointerFromAddress(newPointerVar:GetAddress())
+    if err2 ~= nil then
+        warn(err)
+    end
+
+    local function summarize()
+        print("p1:", newPointerVar.Value)
+        print("p2:", newPointerVar.Value)
+    end
+
+    summarize()
+    newPointerVar.Value = 100
+    summarize()
+end
