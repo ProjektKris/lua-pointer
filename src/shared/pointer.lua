d3290dda546b36de85ab9a419b5d56513f4b1e8b
ProjektKris <rbxkrisxyz@gmail.com>
2021-05-22 14:23:23 +0700

    added pointer addresses

--- src/shared/pointer.lua
+++ src/shared/pointer.lua
@@ -1,3 +1,4 @@
+--!nonstrict
 --[[
 	Author: @ProjektKris
 	lua pointer, takes advantage of table behaviour that doesn't clone, instead points
@@ -7,11 +8,31 @@
 export type Pointer = {
 	Value: any,
 	New: (any) -> (Pointer, string),
+	GetAddress: () -> string,
 	Delete: () -> ()
 }
 
+local pointers = {}
 local pointer = {}
 pointer.__index = pointer
+
+function pointer.GetPointerFromAddress(address: string): (Pointer, string)
+	local p: Pointer
+	local s: boolean, err: string = pcall(function()
+		if pointers[address] then
+			p = pointers[address]
+		else
+			error("pointer not found")
+		end
+	end)
+	if not s then
+		-- error
+		return nil, err
+	else
+		-- success
+		return p, nil
+	end
+end
 
 function pointer.New(value: any): (Pointer, string)
 	local newPointer: Pointer
@@ -20,6 +41,7 @@ function pointer.New(value: any): (Pointer, string)
 			Value = value
 		}
 		setmetatable(newPointer, pointer)
+		pointers[newPointer:GetAddress()] = newPointer
 	end)
 	if not s then
 		-- error
@@ -27,6 +49,10 @@ function pointer.New(value: any): (Pointer, string)
 	else
 		return newPointer, nil
 	end
+end
+
+function pointer:GetAddress(): string
+	return tostring(self)
 end
 
 function pointer:Delete()
